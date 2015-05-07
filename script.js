164b16e45e4cc5476537ec27960ea99f0e4bd2e9
Jennifer Mah <jen.mah@hotmail.com>
2015-05-08 00:13:55 +0100

    made input boxes submit information from one button rather than four

--- script.js
+++ script.js
@@ -1,97 +1,93 @@
   var yourTripArray = [];
-  var startStation = '';
-  var endStation = '';
-  var startLine = '';
-  var endLine = '';
+  var startStation = [];
+  var endStation = [];
+  var startLine = [];
+  var endLine = [];
 
 $(document).ready(function() {
 
-  $('#button1').on('click', function(event) {
+  $('#button4').on('click', function(event) {
     var yourTrip = $('.your-trip');
     startStation = $('#start-station').val();
-    yourTrip.append('<li>' + 'You are starting at ' + startStation + '</li>');
+    yourTrip.append('<li>' + 'you are starting at ' + startStation + '</li>');
     yourTripArray.push(startStation);
     localStorage.setItem('startingStation', JSON.stringify(yourTripArray));
     $('#start-station placeholder').val('');
   })
-    $('#button2').on('click', function(event) {
+    $('#button4').on('click', function(event) {
     var yourTrip = $('.your-trip');
     endStation = $('#end-station').val();
-    yourTrip.append('<li>' + 'You are ending at ' + endStation + '</li>');
+    yourTrip.append('<li>' + 'you are ending at ' + endStation + '</li>');
     yourTripArray.push(endStation);
     localStorage.setItem('endingStation', JSON.stringify(yourTripArray));
   })
-    $('#button3').on('click', function(event) {
+    $('#button4').on('click', function(event) {
     var yourTrip = $('.your-trip');
     startLine = $('#start-line').val();
-    yourTrip.append('<li>' + 'Your starting line is ' + startLine + '</li>');
+    yourTrip.append('<li>' + 'your starting line is ' + startLine + '</li>');
     yourTripArray.push(startLine);
     localStorage.setItem('startingLine', JSON.stringify(yourTripArray));
   })
     $('#button4').on('click', function(event) {
     var yourTrip = $('.your-trip');
     endLine = $('#end-line').val();
-    yourTrip.append('<li>' + 'Your ending line is ' + endLine + '</li>');
+    yourTrip.append('<li>' + 'your ending line is ' + endLine + '</li>');
     yourTripArray.push(endLine);
     localStorage.setItem('endingLine', JSON.stringify(yourTripArray));
   });
-});
+// });
 
 
 
 
-// var mta = {
-//   'n': ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'],
-//   'l': ['8th-l', '6th', 'us', '3rd', '1st'],
-//   's': ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
-// };
+var mta = {
+  'n': ['ts', '34th', '28th-n', '23rd-n', 'us', '8th-n'],
+  'l': ['8th-l', '6th', 'us', '3rd', '1st'],
+  's': ['gc', '33rd', '28th-s', '23rd-s', 'us', 'ap']
+};
 
-// function menu() {
-//   var response = prompt('Welcome to MTA!!! \n(m)ta or (q)uit');
-//   while(response !== 'q') {
-//     var userInput = getUserInput();
-//     var tripLength = calculateStops(userInput);
-//     alert('Your trip length is ' + tripLength + ' stops!');
-//   }
-// }
+function menu() {
+    var userInput = getUserInput();
+    // var tripLength = calculateStops(userInput);
+    alert('Your trip length is ' + tripLength + ' stops!');
+  }
 
-// function getUserInput() {
-//   var startTrain = prompt('What train do you want to get on at: \n' + Object.keys(mta).join(', ') + '?');
-//   var firstStop = prompt('Which stop: ' + mta[startTrain].join(', '));
-//   var stopTrain = prompt('What train do you want to get on at: \n' + Object.keys(mta).join(', ') + '?');
-//   var lastStop = prompt('Which stop: ' + mta[stopTrain].join(', '));
+function getUserInput() {
+    startLine = startLine;
+    startStation = startStation;
+    endLine = endLine;
+    endStation = endStation;
   
-//   return {startTrain: startTrain, 
-//           firstStop: firstStop,
-//           stopTrain: stopTrain,
-//           lastStop: lastStop};
-// }
+  return {startLine: startLine, 
+          startStation: startStation,
+          endLine: endLine,
+          endStation: endStation};
+}
 
 // function calculateStops(userInput) {
-//   return userInput.startTrain === userInput.stopTrain ? sameLine(userInput) : differentLines(userInput);
+//   return userInput.startLine === userInput.endLine ? sameLine(userInput) : differentLines(userInput);
 // }
 
 // function differentLines(userInput) {
-//   var intersection = mta[userInput.startTrain].filter(function(stop) {
-//       return mta[userInput.stopTrain].indexOf(stop) != -1;
+//   var intersection = mta[userInput.startLine].filter(function(stop) {
+//       return mta[userInput.endLine].indexOf(stop) != -1;
 //     })[0];
 
-//   var startTrainIndex = mta[userInput.startTrain].indexOf(userInput.firstStop);
-//   var startTrainIntersectionIndex = mta[userInput.startTrain].indexOf(intersection);
+//   var startTrainIndex = mta[userInput.startLine].indexOf(userInput.startStation);
+//   var startTrainIntersectionIndex = mta[userInput.startLine].indexOf(intersection);
 //   var firstTripLength = Math.abs(startTrainIndex - startTrainIntersectionIndex);
 
-//   var stopTrainIndex = mta[userInput.stopTrain].indexOf(userInput.lastStop);
-//   var stopTrainIntersectionIndex = mta[userInput.stopTrain].indexOf(intersection);
+//   var stopTrainIndex = mta[userInput.endLine].indexOf(userInput.lastStop);
+//   var stopTrainIntersectionIndex = mta[userInput.endLine].indexOf(intersection);
 //   var lastTripLength = Math.abs(stopTrainIndex - stopTrainIntersectionIndex);
 
 //   return firstTripLength + lastTripLength;
 // }
 
-// function sameLine(userInput) {
-//   return Math.abs(mta[userInput.startTrain].indexOf(userInput.firstStop) - mta[userInput.stopTrain].indexOf(userInput.lastStop));
-// }
+function sameLine(userInput) {
+  return Math.abs(mta[userInput.startLine].indexOf(userInput.startStation) - mta[userInput.endLine].indexOf(userInput.endStation));
+}
 
-// menu();
+menu();
 
-
-// });
+});
